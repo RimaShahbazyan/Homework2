a7c068da794ca2e1ec19f69dbee7ce56588b2898
RimaShahbazyan <44582802+RimaShahbazyan@users.noreply.github.com>
2018-11-08 13:15:37 +0400

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -1,20 +1,23 @@
+
 import random
 
 def guess_the_number(wins, lost):
     number = random.randint(1, 100)
-    print (number)
+  #  print (number)
     guess = input("Enter a number from 1 to 100: ")
-
+    
     while guess!=str(number):
-        if guess=="exit"  or guess== "Exit":
+        if(guess=="exit"):
+            return
+        if guess=="give up":
             print("You lost")
             break
         elif int(guess)>number:
-            guess = input ("Enter a lower number or exit : " )
+            guess = input ("Enter a lower number or give up : " )
         else:
-            guess = input("Enter a higher number or exit : ")
+            guess = input("Enter a higher number or give up : ")
         
-    if guess != "exit" and guess!="Exit":      
+    if guess != "give up" :      
         print ("You won!")
         wins+=1
     else:
